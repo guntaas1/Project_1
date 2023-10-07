ecf7fc4aca7762f15fabba86deb0300f8c451ead
jindjeetcheema <jindjeet2010@gmail.com>
2023-10-06 20:42:16 -0600

    changes made

--- calc.py
+++ calc.py
@@ -1,2 +1,15 @@
-print ("hello world")
-print("check")
+def myAdd(num1 ,num2):
+    total = num1 + num2
+    return total
+
+def mySub(num1 ,num2):
+    total = num1 - num2
+    return total
+
+def myMul(num1 ,num2):
+    total = num1 * num2
+    return total
+
+def myDiv(num1 ,num2):
+    total = (num1 / num2)
+    return total
