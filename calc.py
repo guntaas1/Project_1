870aed8b2ace8ef38ad50518d9742dd1b922ea3f
jindjeetcheema <jindjeet2010@gmail.com>
2023-10-06 21:12:45 -0600

    changes made

--- calc.py
+++ calc.py
@@ -1,15 +1,17 @@
-def myAdd(num1 ,num2):
+
+def myAdd(num1 ,num2):   #function for Addition
     total = num1 + num2
     return total
 
-def mySub(num1 ,num2):
+def mySub(num1 ,num2):   #function for Subtraction
     total = num1 - num2
     return total
 
-def myMul(num1 ,num2):
+def myMul(num1 ,num2):   #function for Multiplication
     total = num1 * num2
     return total
 
-def myDiv(num1 ,num2):
+def myDiv(num1 ,num2):   #function for Division
     total = (num1 / num2)
     return total
+
